184de437e7a558b59e3f575fa998b9e5b3c47de4
VishnuNakade <vishnunakade@gmail.com>
2025-06-20 02:15:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const os = require('os');
+const cors = require('cors');
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+
+// Function to get the local IP address
+function getLocalIP() {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const net of interfaces[name]) {
+      if (net.family === 'IPv4' && !net.internal) {
+        return net.address;
+      }
+    }
+  }
+}
+
+app.get('/api/get-ip', (req, res) => {
+  const ip = getLocalIP();
+  res.json({ ip });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
